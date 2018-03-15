762dddfcd2f48e240ff08cb5ff8a98904c8ed762
Roberto <Roberto@Roberto-PC.homenet.telecomitalia.it>
2018-03-15 17:37:15 +0100

    Prima parte

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,106 @@
 package it.polito.tdp.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	
+	private List <Voto> voti;
+	
+	public Libretto() {
+		this.voti=new ArrayList<>();
+	}
+	
+	public Libretto (List<Voto> voti) {
+//  #1 ShallowCopy--> stesso oggetto (lista), una modifica ad una lista comporta la modifica anche all'altra lista pur essendo in due oggetti differenti
+//  this.voti=voti;
+		
+//  #2 ShallowCopy--> stesso oggetto (lista), una modifica ad una lista comporta la modifica anche all'altra lista pur essendo in due oggetti differenti
+//  this.voti= new ArrayList<Voto>();
+		
+//  #3 ShallowCopy--> stesso oggetto (lista), una modifica ad una lista comporta la modifica anche all'altra lista pur essendo in due oggetti differenti
+//  this.voti=new ArrayList<Voto>();
+//  for(Voto v: voti){
+//		this.voti.add(v);
+//	}
+		
+//  #4 DeepCopy--> due oggetti differenti (lista)
+//  this.voti=new ArrayList<Voto>();
+//  for(Voto v: voti){
+//			this.voti.add(new Voto(v.getEsame(), v.getVoto()));
+//		}
+//  che sarebbe a dire 
+
+		this.voti=new ArrayList<Voto>();
+		this.voti=voti; //l'uguale permette ciò
+	}
+	
+	public void add(Voto v) {
+		if (this.voti.contains(v)) { //per fare questo ho dovtuo riscrivere equals and hashcode di voto
+			System.out.println("Il voto esiste già");
+			return;
+		}
+		for(Voto voto: this.voti) {
+			if(v.getEsame().equals(voto.getEsame())){
+				System.out.println("Trovato lo stesso esame, aggiorno il voto");
+				voto.setVoto(v.getVoto());
+				return;
+			}
+			
+		}
+		this.voti.add(v);
+	}
+	
+	public void stampa() {
+		System.out.println("Ci sono "+voti.size()+" voti");
+		for(Voto v:this.voti) {
+			System.out.println(v);
+		}
+	}
+	
+	public void stampa25() {
+		for(Voto v: voti) {
+			if(v.getVoto()==25) {
+				System.out.println(v);
+			}
+		}
+	}
+	
+	
+	public List<Voto> getVoti()
+	{
+		return this.voti;
+	}
+	
+	
+	@Override
+	public String toString() {
+		StringBuilder sb=new StringBuilder(); //Serve per concatenare più stringhe insieme
+		sb.append("Ci sono "+voti.size()+"\n");
+		for(Voto v:this.voti) {
+			sb.append(v.toString());
+			sb.append("\n");
+		}
+	return sb.toString(); 
+	}
+
+	public String getMarkfromClass(String nomeCorso) {
+		for(Voto v: this.voti) {
+			if(v.getEsame().toLowerCase().equals(nomeCorso.toLowerCase()))
+				return String.valueOf(v.getVoto());
+		}
+		return "Class name not found";
+	}
+
+	public void alzaVoti() {
+		for(Voto v: this.voti) {
+			if(v.getVoto()>= 24)
+				v.setVoto(v.getVoto()+2);
+			else if(v.getVoto()>=18)
+				v.setVoto(v.getVoto()+1);
+		}
+	}
+
+	
 	
 }
--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,88 @@
 package it.polito.tdp.libretto;
 
-public class Voto {
+public class Voto implements Comparable<Voto>{
+	
+	
+	
+	public Voto(String esame, int voto) {
+		super();
+		this.esame = esame;
+		this.voto = voto;
+	}
+	
+	
+	
+	private String esame;
+	private int voto;
+	//private LocalDate data;
+
+	
+	
+	
+	
+	
+	//getter and setter
+	public String getEsame() {
+		return esame;
+	}
+	public void setEsame(String esame) {
+		this.esame = esame;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	
+ 
+	
+	
+	public void setVoto(int voto) {
+		
+		if(voto<18 || voto>34) {
+			throw new IllegalArgumentException("Numero non ammesso!!");
+		 }
+		this.voto=voto;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Voto esame=" + esame + ", voto=" + voto;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((esame == null) ? 0 : esame.hashCode());
+		result = prime * result + voto;
+		return result;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voto other = (Voto) obj;
+		if (esame == null) {
+			if (other.esame != null)
+				return false;
+		} else if (!esame.equals(other.esame))
+			return false;
+		if (voto != other.voto)
+			return false;
+		return true;
+	}
+	@Override
+	public int compareTo(Voto v) {
+		return this.esame.compareTo(v.getEsame());
+	}
+	
+	
 	
 }
